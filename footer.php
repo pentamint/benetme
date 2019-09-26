7a4585fee030c79dc1ed2b1782f3df8fc65d98cf
Michael Park <mpark@pentamint.com>
2019-09-27 02:12:13 +0900

    UPDATE: woocommerce design updated

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -10,90 +11,92 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<div id="top-footer" class="footer-wrapper">
-		<div class="container">
-			<div class="row">
-				<!-- <div class="top-footer-nav col-md-9"> -->
-					<?php
-						wp_nav_menu( array(
-							'theme_location'    => 'top-footer',
-							'container_class'   => 'collapse navbar-collapse col-md-9',
-							'container_id'      => 'top-footer-nav',
-							'menu_class'        => 'nav navbar-nav',
-							'menu_id'			=> 'top-footer-menu',
-							'depth'             => 2,
-							'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-							'walker'            => new wp_bootstrap_navwalker(),)
-						);
-					?>
-				<!-- </div> -->
-				<div class="top-footer-social-links col-md-3">
-					<?php
-						if(is_active_sidebar('top-footer-widget-1')){
-							dynamic_sidebar('top-footer-widget-1');
-						}
-					?>
-				</div>
+<div id="top-footer" class="footer-wrapper">
+	<div class="container">
+		<div class="row">
+			<!-- <div class="top-footer-nav col-md-9"> -->
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location'    => 'top-footer',
+					'container_class'   => 'collapse navbar-collapse col-md-9',
+					'container_id'      => 'top-footer-nav',
+					'menu_class'        => 'nav navbar-nav',
+					'menu_id'			=> 'top-footer-menu',
+					'depth'             => 2,
+					'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+					'walker'            => new wp_bootstrap_navwalker(),
+				)
+			);
+			?>
+			<!-- </div> -->
+			<div class="top-footer-social-links col-md-3">
+				<?php
+				if (is_active_sidebar('top-footer-widget-1')) {
+					dynamic_sidebar('top-footer-widget-1');
+				}
+				?>
 			</div>
 		</div>
 	</div>
+</div>
 
-	<div id="main-footer" class="footer-wrapper">
-		<div class="container">
-			<footer id="footer-sidebar" class="row widget-area">
-				<div id="footer-sidebar1" class="col-md-3">
-					<?php
-						if(is_active_sidebar('footer-sidebar-1')){
-							dynamic_sidebar('footer-sidebar-1');
-						}
-					?>
-				</div>
-				<div id="footer-sidebar2" class="col-md-3">
-					<?php
-						if(is_active_sidebar('footer-sidebar-2')){
-							dynamic_sidebar('footer-sidebar-2');
-						}
-					?>
-				</div>
-				<div id="footer-sidebar3" class="col-md-3">
-					<?php
-						if(is_active_sidebar('footer-sidebar-3')){
-							dynamic_sidebar('footer-sidebar-3');
-						}
-					?>
-				</div>
-				<div id="footer-sidebar4" class="col-md-3">
-					<?php
-						if(is_active_sidebar('footer-sidebar-4')){
-							dynamic_sidebar('footer-sidebar-4');
-						}
-					?>
-				</div>
-			</footer>
-		</div><!-- .container -->
-	</div><!-- .footer-wrapper -->
+<div id="main-footer" class="footer-wrapper">
+	<div class="container">
+		<footer id="footer-sidebar" class="row widget-area">
+			<div id="footer-sidebar1" class="col-md-3">
+				<?php
+				if (is_active_sidebar('footer-sidebar-1')) {
+					dynamic_sidebar('footer-sidebar-1');
+				}
+				?>
+			</div>
+			<div id="footer-sidebar2" class="col-md-3">
+				<?php
+				if (is_active_sidebar('footer-sidebar-2')) {
+					dynamic_sidebar('footer-sidebar-2');
+				}
+				?>
+			</div>
+			<div id="footer-sidebar3" class="col-md-3">
+				<?php
+				if (is_active_sidebar('footer-sidebar-3')) {
+					dynamic_sidebar('footer-sidebar-3');
+				}
+				?>
+			</div>
+			<div id="footer-sidebar4" class="col-md-3">
+				<?php
+				if (is_active_sidebar('footer-sidebar-4')) {
+					dynamic_sidebar('footer-sidebar-4');
+				}
+				?>
+			</div>
+		</footer>
+	</div><!-- .container -->
+</div><!-- .footer-wrapper -->
 
-	<div id="bottom-footer" class="footer-wrapper">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-9">
-					<footer id="colophon" class="site-footer" role="contentinfo">
-						<div class="site-info">
-							회사주소<br> All Rights Reserved © <?php echo date("Y");?> <span class="enfont">회사명</span>
-							<span class="sep"> | </span>
-						    
-							Developed by <a href="https://www.pentamint.com">Pentamint Co., Ltd.</a>.
-						</div><!-- .site-info -->
-					</footer><!-- #colophon -->
-				</div><!-- .col-md-9 -->
+<div id="bottom-footer" class="footer-wrapper">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-9">
+				<footer id="colophon" class="site-footer" role="contentinfo">
+					<div class="site-info">
+						All Rights Reserved © <?php echo date("Y"); ?> <span class="enfont">Benetme</span>
+						<span class="sep"> | </span>
+
+						Developed by <a href="https://www.pentamint.com">Pentamint Co., Ltd.</a>.
+					</div><!-- .site-info -->
+				</footer><!-- #colophon -->
+			</div><!-- .col-md-9 -->
+			<?php if (has_nav_menu('colophon')) : ?>
 				<div class="col-md-3">
 					<!-- Default dropup button -->
-
 					<div class="btn-group dropup">
 						<button type="button" class="btn btn-tertiary btn-colophon-toggle" data-toggle="collapse" data-target="#colophon-brand-nav">
 							Link Sites
@@ -101,26 +104,30 @@ defined( 'ABSPATH' ) || exit;
 						<div class="collapse dropdown-menu" id="colophon-brand-nav">
 							<!-- Dropdown menu links -->
 							<?php
-								wp_nav_menu( array(
-									'theme_location'    => 'colophon',
-									'container_class'   => 'collapse navbar-collapse',
-									'container_id'      => 'colophon-brand-nav',
-									'menu_class'        => 'nav navbar-nav',
-									'menu_id'			=> 'colophon-brand-menu',
-									'depth'             => 0,
-									'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-									'walker'            => new wp_bootstrap_navwalker(),)
+								wp_nav_menu(
+									array(
+										'theme_location'    => 'colophon',
+										'container_class'   => 'collapse navbar-collapse',
+										'container_id'      => 'colophon-brand-nav',
+										'menu_class'        => 'nav navbar-nav',
+										'menu_id'			=> 'colophon-brand-menu',
+										'depth'             => 0,
+										'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+										'walker'            => new wp_bootstrap_navwalker(),
+									)
 								);
-							?>
+								?>
 						</div><!-- .dropdown-menu -->
 					</div><!-- .btn-group -->
 				</div><!-- .col-md-3 -->
-			</div><!-- .row -->
-		</div><!-- .container -->
-	</div><!-- .colophon-wrapper -->
+			<?php endif;  ?>
+		</div><!-- .row -->
+	</div><!-- .container -->
+</div><!-- .colophon-wrapper -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
